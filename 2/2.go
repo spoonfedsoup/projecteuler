9a1ce9b6c98264116c62c3c8211ff730722a366d
spoonfedsoup <spoonfedsoup@gmail.com>
2016-05-11 22:25:29 -0400

    Sum of integers less than 1000 that are divisible by 3 or 5

---
+++ 2/2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	p := 0
+	q := 1
+	sum := 0
+
+	for i := 0; p <= 4000000; i++ { //calculates nth Fib
+		p, q = q, p+q
+		if p%2==0{   //checks for eveness
+			sum += p  //adds if even
+		}
+	}
+	fmt.Println(sum)
+}
+//I wanted to use the closed-form Binet's Formula to calculate
+//the nth fibonnaci number, but type casting was cumbersome.
+//The complexity of recursion is much higher, but it's kind of cool.
